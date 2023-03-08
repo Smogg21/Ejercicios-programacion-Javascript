e7caaf4bea5fa091c5c94b0ff73290ad694e3120
Smogg21 <spazzaglia98@gmail.com>
2023-03-08 17:42:12 -0500

    Added everything needed until 84.pt 2

--- Js/teclado.js
+++ Js/teclado.js
@@ -2,4 +2,18 @@ const d = document;
 
 export function shortcuts(e){
     console.log(e);
+
+    if(e.key === "a" && e.altKey){
+        alert("Haz lanzado una alerta con el teclado");
+
+    }
+
+    if(e.key === "c" && e.altKey){
+        confirm("Haz lanzado una confirmación con el teclado");
+        
+    }
+
+    if(e.key === "v" && e.altKey){
+        prompt("Haz lanzado un aviso con el teclado");
+    }
 }
