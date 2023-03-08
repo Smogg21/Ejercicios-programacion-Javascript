1ccfb694682718400ad6c6a37701db9c5cd69c44
Smogg21 <spazzaglia98@gmail.com>
2023-03-08 17:32:40 -0500

    Added everything needed until 84.

--- Js/index_dom.js
+++ Js/index_dom.js
@@ -1,5 +1,6 @@
 import hamburguerMenu from "./menu-hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
+import { shortcuts } from "./teclado.js";
 
 const d = document;
 
@@ -7,5 +8,9 @@ d.addEventListener("DOMContentLoaded", (e)=>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj", "#desactivar-reloj")
     alarm("../assets/aestetic-alarm.mp3","#activar-alarma","#desactivar-alarma");
+})
+
+d.addEventListener("keydown", e=>{
+    shortcuts(e)
 })
 
---
+++ Js/teclado.js
@@ -0,0 +1,5 @@
+const d = document;
+
+export function shortcuts(e){
+    console.log(e);
+}
