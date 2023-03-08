d213c40582e5ca4ae9c4b9f01e0ff61f30538b7b
Smogg21 <spazzaglia98@gmail.com>
2023-03-08 16:02:25 -0500

    Added alarm functionality

--- Js/index_dom.js
+++ Js/index_dom.js
@@ -6,5 +6,6 @@ const d = document;
 d.addEventListener("DOMContentLoaded", (e)=>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj", "#desactivar-reloj")
+    alarm("../assets/aestetic-alarm.mp3","#activar-alarma","#desactivar-alarma");
 })
 
