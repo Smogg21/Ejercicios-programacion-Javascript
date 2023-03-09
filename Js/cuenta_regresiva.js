01766099c59ed071f2c54893578bcaceaaa12c36
Smogg21 <spazzaglia98@gmail.com>
2023-03-09 16:10:18 -0500

    Added countdown functionality

---
+++ Js/cuenta_regresiva.js
@@ -0,0 +1,32 @@
+
+const d = document;
+
+export default function countdown(id,limitDate,finalMessage){
+    const $countdown = d.getElementById(id);
+    const countdownDate = new Date(limitDate).getTime();
+
+    let countdownTempo = setInterval(() => {
+        let now = new Date().getTime();
+        let limitTime = countdownDate - now,
+        days = Math.floor(limitTime/(1000 * 60 * 60 * 24)),
+        hours = 
+        ("0" +
+        Math.floor(limitTime%(1000 * 60 * 60 * 24) / (1000 * 60 * 60))).slice(-2),
+        minutes = 
+        ("0" +
+        Math.floor(limitTime%(1000 * 60 * 60) / (1000 * 60))).slice(-2),
+        seconds =  
+        ("0" +
+        Math.floor(limitTime%(1000 * 60) / (1000))).slice(-2);
+
+        $countdown.innerHTML =  `<h3>Faltan: ${days} días ${hours} horas ${minutes} minutos y ${seconds} segundos para el y2k 2.0.</h3>`
+        
+
+        if (limitTime < 0){
+            clearInterval(countdownTempo);
+            $countdown.innerHTML =  `<h3>${finalMessage}</h3>`
+
+        }
+    }, 1000);
+
+}
--- Js/index_dom.js
+++ Js/index_dom.js
@@ -1,6 +1,8 @@
+import countdown from "./cuenta_regresiva.js";
 import hamburguerMenu from "./menu-hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall,shortcuts } from "./teclado.js";
+
 
 const d = document;
 
@@ -8,6 +10,7 @@ d.addEventListener("DOMContentLoaded", (e)=>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj", "#desactivar-reloj")
     alarm("../assets/aestetic-alarm.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown","February 7, 2038 06:28:16","Feliz y2k 2.0")
 })
 
 d.addEventListener("keydown", e=>{
