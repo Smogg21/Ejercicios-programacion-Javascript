b8be8a2411a09fc9d959420c27023d00611a5a5b
Smogg21 <spazzaglia98@gmail.com>
2023-03-15 16:37:39 -0500

    Added web size responsive tester

--- Js/index_dom.js
+++ Js/index_dom.js
@@ -2,6 +2,7 @@ import scrollTopButton from "./boton_scroll.js";
 import countdown from "./cuenta_regresiva.js";
 import hamburguerMenu from "./menu-hamburguesa.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsive.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall, shortcuts } from "./teclado.js";
 import darkTheme from "./tema-oscuro.js";
@@ -30,6 +31,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     `<a href="https://goo.gl/maps/wMytTKRMoHfgcvoL7" target="_blank" rel="noopener">Ver Mapa</a>`,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8590149181446!2d-77.03269023403675!3d-12.121797396603965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c819aaa84439%3A0x52deb1f4f48d8bb2!2sParque%20Kennedy%2C%20Miraflores%2015074!5e0!3m2!1ses-419!2spe!4v1678830995186!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
+
+responsiveTester("responsive-tester")
 });
 
 d.addEventListener("keydown", (e) => {
